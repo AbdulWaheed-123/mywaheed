fe39a698e655e0d8800cfa56b7884bc63ba61bb0
abdulwaheed <waheed.shaikh003@gmail.com>
2022-12-11 18:50:23 +0500

    updated

--- src/component/SubComponent.js
+++ src/component/SubComponent.js
@@ -2,7 +2,9 @@ import './SubComponent.css'
 
 function SubComponent(){
     return (
-    <p className="sub">Ali is in SubComponent</p>
+    <><p className="sub">Ali is in SubComponent</p>
+
+    <p>Updates show on netlify</p></>
     );
 }
 export default SubComponent;
